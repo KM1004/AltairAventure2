94becc7aad671037b62980a1ed1b500f6d7bd838
Dinkleyv <113431474+Dinkleyv@users.noreply.github.com>
2026-06-13 15:02:02 +1200

    LevelScene

--- LevelScenes/Level 3.gd
+++ LevelScenes/Level 3.gd
@@ -1,18 +1,26 @@
 extends Node2D
 
+onready var player = $Player
+onready var water = $water
+onready var water_timer = $WaterTimer
 
-#onready var player = $Player
-#onready var main_camera = $MainCamera
+var player_in_water = false
 
+func _process(delta):
+	var cell = water.world_to_map(player.global_position)
+	# Returns -1 if there is no tile
+	var on_water = water.get_cellv(cell) != -1
 
+	if on_water and !player_in_water:
+		player_in_water = true
+		water_timer.start()
 
-# Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#player.died.connect(_on_player_died)
-	#player.camera_remote_transform.remote_path = main_camera.get_path()
-#	pass # Replace with function body.
+	elif !on_water and player_in_water:
+		player_in_water = false
+		water_timer.stop()
+		
 
-
-#func _on_player_died();
-	#print("GameOver")
-	#get_tree().create_timer(3).timeout.connect(get_tree().reload_current_scene)
+func _on_WaterTimer_timeout():
+	if player_in_water:
+		player.lives -= 1
+		#print("Took water damage! Lives:", player.lives)

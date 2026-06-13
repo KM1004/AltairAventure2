0643cc6f23d93c2361c3d86a52210a0cfddc2923
Dinkleyv <113431474+Dinkleyv@users.noreply.github.com>
2026-06-13 14:49:07 +1200

    Update Slime

--- EnemiesScenes/Slime.gd
+++ EnemiesScenes/Slime.gd
@@ -1,23 +1,3 @@
 extends KinematicBody2D
 class_name Enemy
 
-var player = null
-#var player: Player = null
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-
-
-func _on_Player_detector_body_entered(body):
-#	if body is Player:
-#		if player == null:
-#			player = body
-			print("found the player")
-	
-
-
-func _on_Player_detector_body_exited(body):
-	pass # Replace with function body.

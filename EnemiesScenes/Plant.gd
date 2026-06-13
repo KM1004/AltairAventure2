060273aa037cbb5b132b28a9a9b90b4588a2e7ee
Dinkleyv <113431474+Dinkleyv@users.noreply.github.com>
2026-06-13 14:59:48 +1200

    slime/plant

--- EnemiesScenes/Plant.gd
+++ EnemiesScenes/Plant.gd
@@ -1,1 +1,142 @@
 extends KinematicBody2D
+
+#const UP = Vector2(0, -1)
+const SPEED = 200
+#const GRAVITY = 20
+#const JUMP_HEIGHT = -600
+
+var motion = Vector2()
+var altair = "Player"
+enum{
+	IDLE,
+	ATTACK,
+	HURT,
+	DEAD
+}
+	
+var state = IDLE
+
+var player = null
+var player_detected = false
+var player_in_attack_range = false
+var facing = "front"
+
+onready var sprite = $AnimatedSprite
+
+
+
+
+func _on_AnimatedSprite_animation_finished():
+	if state == ATTACK:
+		state = IDLE
+
+
+
+
+
+func _on_DetectArea_body_entered(body):
+	if body.name == altair:
+		player = body
+		player_detected = true
+		
+
+func _on_DetectArea_body_exited(body):
+	if body.name == altair:
+		player_detected = false
+		player_in_attack_range = false
+		player =  null
+		state = IDLE
+	
+
+
+func _on_AttackRange_body_entered(body):
+	if body.name == altair:
+		print(body.name)
+		player_in_attack_range = true
+
+
+func _on_AttackRange_body_exited(body):
+	if body.name == altair:
+		player_in_attack_range = false
+		
+
+func _process(delta):
+	
+	if state == DEAD:
+		return
+	
+	if player_detected and player:
+		face_player()
+		
+		if player_in_attack_range:
+			state = ATTACK
+		else:
+			state = IDLE
+	else:
+		state = IDLE
+		
+	update_animation()
+	
+func face_player():
+	var dir = player.global_position - global_position
+	if abs(dir.x) > abs(dir.y):
+		if dir.x > 0:
+			facing = "right"
+		else:
+			facing = "left"
+	else:
+		if dir.y > 0:
+			facing = "front"
+		else:
+			facing = "back"
+
+
+func update_animation():
+
+	match state:
+		IDLE:
+			match facing:
+				"left":
+					sprite.play("l_idle")
+				"right":
+					sprite.play("r_idle")
+				"front":
+					sprite.play("f_idle")
+				"back":
+					sprite.play("b_idle")
+
+		ATTACK:
+			match facing:
+				"left":
+					sprite.play("l_attack")
+				"right":
+					sprite.play("r_attack")
+				"front":
+					print(state)
+					sprite.play("f_attack")
+				"back":
+					sprite.play("b_attack")
+
+		HURT:
+			match facing:
+				"left":
+					sprite.play("l_hurt")
+				"right":
+					sprite.play("r_hurt")
+				"front":
+					sprite.play("f_hurt")
+				"back":
+					sprite.play("b_hurt")
+
+		DEAD:
+			match facing:
+				"left":
+					sprite.play("l_die")
+				"right":
+					sprite.play("r_die")
+				"front":
+					sprite.play("f_die")
+				"back":
+					sprite.play("b_die")
+		
+
--- EnemiesScenes/Slime.gd
+++ EnemiesScenes/Slime.gd
@@ -1,3 +1,66 @@
 extends KinematicBody2D
-class_name Enemy
 
+var player = null
+var energy = 2
+
+var speed = 80
+var player_in_range = false
+
+
+func _ready():
+	pass
+
+
+func _physics_process(delta):
+
+	if energy <= 0:
+		$AnimatedSprite.play("death")
+		queue_free()
+		return
+
+	if player != null:
+
+		var direction = (player.global_position - global_position).normalized()
+
+		move_and_slide(direction * speed)
+
+		# Animations
+		if abs(direction.x) > abs(direction.y):
+			$AnimatedSprite.play("s_walk")
+			$AnimatedSprite.flip_h = direction.x < 0
+		elif direction.y > 0:
+			$AnimatedSprite.play("d_walk")
+		else:
+			$AnimatedSprite.play("u_walk")
+
+
+func _on_AttackArea_body_entered(body):
+	if body.name == "Player":
+		player = body
+		#subtract player's lives
+
+# ============================
+# PLAYER DETECTION
+# ============================
+
+func _on_Player_detector_body_entered(body):
+	if body.name == "Player":
+		player = body
+		player_in_range = true
+		print("found the player")
+
+
+func _on_Player_detector_body_exited(body):
+	if body == player:
+		player = null
+		player_in_range = false
+
+
+# ============================
+# ARROW / SWORD DAMAGE
+# ============================
+
+func _on_Area2D_area_entered(area):
+	if area.name == "player_arrow" or area.name == "player_sword":
+		energy -= 1
+		area.queue_free()
